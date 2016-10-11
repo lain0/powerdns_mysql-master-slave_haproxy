440ff2dcaef3adaa79bb9b48fffb5426d9dd7675
lain0 <trampampushe4ka@gmail.com>
2016-10-11 10:40:32 +0300

    Read me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # powerdns_mysql-master-slave_haproxy
+
+./dodon.sh
+docker-compose build && docker-compose up
+
+.docoff.sh
+docker-compose stop && docker-compose rm -f
--- mysql_slave/slave_test.sh
+++ mysql_slave/slave_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-printf "\nSTART SLAVE; SHOW VARIABLES LIKE 'server_id';\n"
-printf "\nSHOW SLAVE STATUS\G SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1; SLAVE START; \n"
+# slave_test.sh
+# printf "\nSTART SLAVE; SHOW VARIABLES LIKE 'server_id';\n"
+mysql -uroot -psecretrootpassword -h mysql_slave -e "SHOW SLAVE STATUS\G SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1;"
 mysql -uroot -psecretrootpassword -h mysql_slave -e "START SLAVE;"
